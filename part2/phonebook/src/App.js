2e7352fa63a7b8a6f469ab642e92ed4cfd63c52c
sansx <xiathxth@gmail.com>
2020-09-12 21:07:37 +0800

    get initital data from server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
 
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
@@ -6,22 +7,20 @@ import Persons from "./components/Persons"
 
 const App = () => {
   const [persons, setPersons] = useState([])
-  // const [persons, setPersons] = useState([
-  //   { name: 'Arto Hellas', number: '040-123456' },
-  //   { name: 'Ada Lovelace', number: '39-44-5323523' },
-  //   { name: 'Dan Abramov', number: '12-43-234345' },
-  //   { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  // ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterVal, setFilterVal] = useState('')
 
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(res => setPersons(res.data))
+  }, [])
+
   const submitFn = (e) => {
     e.preventDefault();
-    
-    persons.find(person => person.name === newName) ? 
-    alert(`${newName} is already added to phonebook`) : 
-    setPersons([...persons, { name: newName, number: newNumber }]);
+
+    persons.find(person => person.name === newName) ?
+      alert(`${newName} is already added to phonebook`) :
+      setPersons([...persons, { name: newName, number: newNumber }]);
 
     setNewName('')
     setNewNumber('')
@@ -38,8 +37,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter onChange={generateFn(setFilterVal)} ></Filter>
       <h3>add a new</h3>
-      <PersonForm onSubmit={submitFn} name={newName} nameChange={generateFn(setNewName)} 
-      numberChange={generateFn(setNewNumber)} number={newNumber} ></PersonForm>
+      <PersonForm onSubmit={submitFn} name={newName} nameChange={generateFn(setNewName)}
+        numberChange={generateFn(setNewNumber)} number={newNumber} ></PersonForm>
       <h3>Numbers</h3>
       <Persons persons={persons} filterVal={filterVal} ></Persons>
     </div>
