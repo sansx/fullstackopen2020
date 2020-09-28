119f679df41034a3be7c483cf6287a84293475fa
sansx <xiathxth@gmail.com>
2020-09-28 23:20:37 +0800

    use bcrypt

--- part4/blogList/controllers/login.js
+++ part4/blogList/controllers/login.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
 
--- part4/blogList/controllers/users.js
+++ part4/blogList/controllers/users.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
