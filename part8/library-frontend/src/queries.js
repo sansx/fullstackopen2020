d12b59fc6678f08ff9c064255c7f702300e88366
sansx <xiathxth@gmail.com>
2020-10-13 23:24:48 +0800

    add queries

---
+++ part8/library-frontend/src/queries.js
@@ -0,0 +1,25 @@
+import { gql  } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors{
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query allBooks($author: String, $genre: String){
+    allBooks(author:$author, genre:$genre){
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`
+
+
